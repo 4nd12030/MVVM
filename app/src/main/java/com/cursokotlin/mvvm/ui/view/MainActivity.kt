82325c4b82880020a3abf4c225316024ac870881
Andrea Aguiire <4ndr3420@gmail.com>
2024-08-28 11:45:26 -0600

    Adaptando la capa IU

--- app/src/main/java/com/cursokotlin/mvvm/ui/view/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/mvvm/ui/view/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.cursokotlin.mvvm.ui.view
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import com.cursokotlin.mvvm.databinding.ActivityMainBinding
 import com.cursokotlin.mvvm.ui.viewmodel.QuoteViewModel
@@ -15,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var tvQuote: TextView
     private lateinit var tvAuthor: TextView
+    private lateinit var progress: ProgressBar
     private lateinit var viewContainer: View
 
     //View Model
@@ -31,6 +35,10 @@ class MainActivity : AppCompatActivity() {
         viewContainer = binding.viewContainer
         tvQuote = binding.tvQuote
         tvAuthor = binding.tvAuthor
+        progress = binding.progress
+
+
+        quoteViewModel.onCreate()
 
 
         //View Model -> Suscribe el cambio en la view mediante la funcion observe()
@@ -42,6 +50,12 @@ class MainActivity : AppCompatActivity() {
 //            tvQuote.text = it.quote
 //            tvAuthor.text = it.author
 //        }
+
+
+        //isLoading ea la variable que contiene la respuesta que en este caso es de tipo Boolean puede ser it
+        quoteViewModel.isLoading.observe(this, Observer{ isLoading ->
+            progress.isVisible = isLoading
+        })
 
         //La accion se detona cada que se toque la pantalla
         viewContainer.setOnClickListener { quoteViewModel.randomQuote() }
--- app/src/main/java/com/cursokotlin/mvvm/ui/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/com/cursokotlin/mvvm/ui/viewmodel/QuoteViewModel.kt
@@ -2,19 +2,42 @@ package com.cursokotlin.mvvm.ui.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cursokotlin.mvvm.data.domain.GetQuotesUseCase
 import com.cursokotlin.mvvm.data.model.QuoteModel
 import com.cursokotlin.mvvm.data.model.QuoteProvider
+import kotlinx.coroutines.launch
 
 class QuoteViewModel: ViewModel() {
     //LiveData es un tipo de dato al cual el activity se puede conectar para saber cuadno hay un cambio en el modelo
     val quoteModel = MutableLiveData<QuoteModel>()
+    val isLoading = MutableLiveData<Boolean>()
+
+    var getQuoseUseCase = GetQuotesUseCase()
 
     //Este metodo sera al que acceda nuestra vista
     fun randomQuote(){
         //Llama al provider para que devuelva una nueva cita
-        val currentQuote = QuoteProvider.random()
+           //val currentQuote = QuoteProvider.random()
         //La nueva cita se añade a la variable livedata con postValue() para que la vista reconozca el cambio y lo pinte
-        quoteModel.postValue(currentQuote)
+           //quoteModel.postValue(currentQuote)
+
+
+
+    }
+
+    fun onCreate() {
+        viewModelScope.launch {
+            isLoading.postValue(true)
+            val result = getQuoseUseCase()
+
+            //Asigna la primera cita a la pantalla cuando el usuario inicia la aplicacion
+            if(result.isNotEmpty()){
+                quoteModel.postValue(result[0])
+                isLoading.postValue(false)
+            }
+
+        }
     }
 
 }
